68ceacbd8845bd487990404929635e21e290ae53
Jonathan Mendes <jonathangmendes.jgm@gmail.com>
2022-10-07 16:37:27 -0300

    Implement Firebase Auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,28 @@
+// import '../styles/globals.css'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import MainContainer from '../components/MainContainer'
+
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <MainContainer>
+//       <Component {...pageProps} />
+//     </MainContainer>
+//   )
+// }
+// export default MyApp
+
+
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import MainContainer from '../components/MainContainer'
+
+import { AuthUserProvider } from '../context/AuthUserContext';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MainContainer>
+    <AuthUserProvider>
       <Component {...pageProps} />
-    </MainContainer>
+      </AuthUserProvider>
   )
 }
+
 export default MyApp
